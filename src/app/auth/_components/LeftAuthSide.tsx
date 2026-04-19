360dc6214b2c3e6c947aff38394f59e14046b9b5
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2026-04-19 16:00:03 -0300

    improve leftauthside

--- src/app/auth/_components/LeftAuthSide.tsx
+++ src/app/auth/_components/LeftAuthSide.tsx
@@ -1,7 +1,9 @@
 export function LeftAuthSide() {
   return (
     <section className="h-screen flex w-[71%] bg-[#0123cd] text-white px-15">
-      <span>Code & Care</span>
+      <span className="text-3xl flex font-semibold tracking-wide mt-28">
+        &lt;/CODE &amp; CARE&gt;
+      </span>
     </section>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -29,14 +19,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn(
-        "h-full",
-        "antialiased",
-        geistSans.variable,
-        geistMono.variable,
-        "font-sans",
-        inter.variable,
-      )}
+      className={cn("h-full", "antialiased", "font-sans", inter.variable)}
     >
       <body className="min-h-full flex flex-col">
         <Toaster />
